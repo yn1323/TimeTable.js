2b1535afaaf26b446444547b23d26de3fdd105b7
yn1323 <yn1323@gmail.com>
2020-01-07 00:26:21 +0900

    for jest to use import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
-        "module": false,
-        "require": false,
-        "process": false,
-        "__dirname": false
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "eqeqeq": "warn",
-        "quotes": ["warn", "single"]
-    }
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
+    "module": false,
+    "require": false,
+    "process": false,
+    "__dirname": false,
+    "describe": false,
+    "test": false,
+    "expect": false
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "eqeqeq": "warn",
+    "quotes": ["warn", "single"]
+  }
 };
