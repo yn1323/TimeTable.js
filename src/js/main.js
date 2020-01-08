f0206a6bae25a4fb7a7de1bb269f3ac476e094e1
yn1323 <yn1323@gmail.com>
2020-01-08 22:45:44 +0900

    Delete files

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import '../scss/main.scss'
-import util from './util.js'
-
-// Place TimeTable Entrance
-window.TimeTable = obj => {
-  console.log(obj)
-}
--- src/js/template.js
+++
@@ -1,8 +0,0 @@
-// Create DOM
-const breakPoint = 896
-const isMobile = window.outerWidth < breakPoint
-
-// Desktop
-let desktop = {}
-
-export default isMobile
